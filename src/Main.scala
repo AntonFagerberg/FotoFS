d92158a1aa96da3fad663d14e963a931c1f0349d
Anton Fagerberg <anton.fagerberg@jayway.com>
2014-02-08 23:09:43 +0100

    Fixed naming conventions.

--- src/Main.scala
+++ src/Main.scala
@@ -2,12 +2,12 @@ import akka.actor.{Actor, Props, ActorSystem}
 import service.Flickr
 
 object Main extends App {
-  val system = ActorSystem("FotoFSSystem")
-  val fotoFS = system.actorOf(Props[FotoFS], name = "FotoFS")
+  val system = ActorSystem("foto-fs-system")
+  val fotoFS = system.actorOf(Props[FotoFS], name = "foto-fs")
 
   class FotoFS extends Actor {
     val filesystem = system.actorOf(Props[Filesystem], name = "filesystem")
-    val flickr = system.actorOf(Props[Flickr], name = "Flickr")
+    val flickr = system.actorOf(Props[Flickr], name = "flickr")
 
     override def receive: Receive = {
       case _ => {}
